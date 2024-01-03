d1c5bd6af5352eeb40ad2843b3992b5625e47892
noblet <appiahpatricknoblet@gmail.com>
2024-01-03 02:51:26 +0000

    added a function in C that inserts a number into a sorted singly linked list.

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a new node with a specified number into
+ * a sorted linked list.
+ * @head: Pointer to the head of the linked list.
+ * @number: The number to be inserted into the linked list.
+ *
+ * Description:
+ * This function adds a new node to a linked list while ensuring
+ * the list remains sorted.
+ * It takes a pointer to the head of the list and the number for
+ * the new node.
+ * The function handles cases such as an empty list,insertion
+ * at the beginning, and finding the correct position for insertion
+ * to maintain sorted order.
+ *
+ * Return: Pointer to the newly inserted node or NULL on failure.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current, *new_node;
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Initialize the new node with the given number */
+	new_node->n = number;
+	current = *head; /* Set 'current' to the head of the list */
+
+	/* is the list empty? insert! */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	if (new_node->n < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Traverse the list to find the correct position for insertion. */
+	while (current->next != NULL && new_node->n > current->next->n)
+		current = current->next;
+
+	/* Insert the new node in the middle or at the end of the list. */
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (node); /* Return the newly inserted node. */
+}
+
