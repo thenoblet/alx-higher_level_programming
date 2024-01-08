3bb6f2afa3cafe67261ebbe836687b449ec99ee3
noblet <appiahpatricknoblet@gmail.com>
2024-01-08 21:19:59 +0000

    added a function that prints elements of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Description: Function prints elements in `my_list`.
+Uses list comprehension to iterate through each element
+in my_list and print it
+"""
+
+
+def print_list_integer(my_list=[]):
+    [print(x) for x in my_list]
