e1c593113145df3d61b25d230fc0d0be44f7c9f8
noblet <appiahpatricknoblet@gmail.com>
2024-01-22 22:56:59 +0000

    FIXED: typo

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,9 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-        return result
-    except (ZeroDivisionError, TypeError, ValueError):
-        return None
+    except (ZeroDivisionError, Exception):
+        pass
     finally:
-        print("Inside result: {}".format(result)
+        print("Inside result: {}".format(result))
+        return result
+
