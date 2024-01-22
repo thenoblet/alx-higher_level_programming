a5e8a9da2c009f44066c8a97eaf2ba5d70add3f9
noblet <appiahpatricknoblet@gmail.com>
2024-01-22 22:22:57 +0000

    fixed typerror

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    excepti(ValueError, TypeError):
+            print()
+            return count
     print()
-    return(count)
+    return count
