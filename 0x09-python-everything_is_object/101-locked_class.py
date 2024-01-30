2fa9db6c71bdbaefa6bdce479ecd9f59c83585db
noblet <appiahpatricknoblet@gmail.com>
2024-01-30 21:33:27 +0000

    fixed errors in magic string and locked class functions

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+
+
 class LockedClass:
-    __slots__ = 'first_name'
+    """
+    A class demonstrating the use of __slots__ to restrict attribute creation.
+
+    Attributes:
+    - first_name (str): A slot for storing the first name of an instance.
+
+    Note:
+    The __slots__ attribute is used to explicitly declare a list of allowed
+    ittribute names.
+    Instances of this class will only be able to have attributes listed in __slots__.
+    This can be useful for memory optimization and to prevent the creation of
+    arbitrary attributes.
+    """
+    __slots__ = ["first_name"]
