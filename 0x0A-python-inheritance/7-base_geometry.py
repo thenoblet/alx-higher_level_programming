504b9715e0007fa47ef152ab4874a508dc953781
noblet <appiahpatricknoblet@gmail.com>
2024-02-05 23:06:55 +0000

    a class BaseGeometry based on 6-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,17 +8,27 @@ class BaseGeometry:
     """
     Class that defines basic geometries.
     """
+
     def area(self):
         """
         Area of a geometry, not implemented by default.
+        Raises:
+            Exception: When area() is not implemented.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Function that validates that value is integer.
+        Args:
+            name (str): The name of the variable to be validated.
+            value (int): The value to be validated.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
         """
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
+
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
