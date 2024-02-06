9f3ca11258d0976bf2b06d4d494f0bce8dfc2b91
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-02-06 22:00:20 +0000

    Update and rename 1-number_of_lines.py to 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Model that contains a function that writes or overwrites to a file."""
+
+
+def write_file(filename="", text""):
+    """
+    Writes or overwrites text to a specified file.
+
+    Parameters:
+    - filename (str): The name of the file to write to.
+    - text (str): The text to be written to the file.
+
+    Returns:
+    int: The number of characters written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
