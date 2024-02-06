c856c7b74a5f74da63dee309e46aee94720d4bea
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-02-06 22:35:18 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
 
 """
-Script that stores arguments as json representations in file add_item.json.
+This script provides functionality to save items to a JSON file.
 """
-from sys import argv
-import os.path
-if not os.path.exists("add_item.json"):
-    with open("add_item.json", "w", encoding='utf-8') as f:
-        f.write("[]")
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-my_list = load_from_json_file("add_item.json")
-argc = len(argv)
-for i in range(1, argc):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+
+
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def save_to_json_file(args, json_file: str = "add_item.json"):
+    """
+    Saves items to a JSON file.
+
+    Parameters:
+    - args (List[str]): List of items to be saved to the JSON file.
+    - json_file (str): The name of the JSON file. Default is "add_item.json".
+
+    Returns:
+    None
+    """
+    new_list = []
+
+    try:
+        new_list = load_from_json_file(json_file)
+    except Exception:
+        save_to_json_file([], json_file)
+
+    for arg in args:
+        new_list.append(arg)
+
+    save_to_json_file(new_list, json_file)
+
+
+if __name__ == "__main__":
+    save_to_json_file(sys.argv[1:])
