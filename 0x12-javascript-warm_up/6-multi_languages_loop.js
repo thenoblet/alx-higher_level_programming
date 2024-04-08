6e1b96786adaf67ec509a8e5e1028b5b5b901087
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-08 23:36:17 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 for (i; i < array.length; i++) {
