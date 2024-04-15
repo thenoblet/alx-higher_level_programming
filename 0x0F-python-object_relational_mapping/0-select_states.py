48bda48b726ad4febea03c3c47176f9670052b8c
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-15 00:44:31 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # This script Lists all states from the database hbtn_0e_0_usa.
-import MySQLdb
-import sys
+
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
