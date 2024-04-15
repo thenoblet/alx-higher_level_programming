4d7b937fff0ea9c914ac0c249d6d15f631c8db97
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-15 00:49:20 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# This script Lists all states starting with N.
+
+"""This script lists all states with a name starting with 'N' from
+the database `hbtn_0e_0_usa`"""
+
 import MySQLdb
 import sys
 if __name__ == "__main__":
