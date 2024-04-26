8d03c91580a820a2317ccae5bbc0c8a6a7465de7
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-26 23:31:56 +0000

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,20 +7,17 @@ This module contains functionality to fetch and print the content
 of a URL provided as a command-line argument.
 """
 
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
 import sys
+import urllib.error
+import urllib.request
+
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        print(f"Usage: {sys.argv[0]} <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    req = Request(url)
 
+    request = urllib.request.Request(url)
     try:
-        with urlopen(req) as response:
-            print(response.read().decode('utf-8'))
-    except HTTPError as e:
-        print(f"Error code: {e.code}")
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
