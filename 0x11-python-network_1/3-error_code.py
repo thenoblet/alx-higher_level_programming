345c018571c7080437e44a4e948ad89e4c3bf857
noblet <appiahpatricknoblet@gmail.com>
2024-04-27 00:07:32 +0100

    ADDED: a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module to fetch and print the content of a URL.
+
+This module contains functionality to fetch and print the content
+of a URL provided as a command-line argument.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
