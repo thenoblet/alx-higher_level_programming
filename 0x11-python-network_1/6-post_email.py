67d66522e607f2a0ff09040588e07a7d2f82db1e
noblet <appiahpatricknoblet@gmail.com>
2024-04-27 01:05:01 +0100

    a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Module to send a POST request with email as a parameter to a
+URL using requests.
+
+This module contains functionality to send a POST request with an
+email parameter to a URL provided as a command-line argument along 
+with the email address using the requests library.
+
+Dependencies:
+    - requests
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response = requests.post(url, data={'email': email})
+    print(response.text)
