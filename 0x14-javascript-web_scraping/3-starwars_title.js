28d35284ec519bfe421be23c86f94d6f41dad246
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-05-21 23:22:52 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const id = process.argv[2];
+
+request(`${url}/${id}`, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(response.body).title);
+  }
 });
