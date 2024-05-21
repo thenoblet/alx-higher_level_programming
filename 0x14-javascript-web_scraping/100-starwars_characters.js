dcf59d7659dd8952e7682e0cb6b9af5794106581
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-05-21 23:18:16 +0000

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,30 +1,21 @@
+#!/usr/bin/node
+
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, (error, response) => {
   if (error) {
     console.error(error);
-    return;
-  }
-
-  const characters = JSON.parse(response.body).characters;
-  const characterPromises = characters.map(character => {
-    return new Promise((resolve, reject) => {
-      request(character, (error, response) => {
+  } else {
+    const characters = JSON.parse(response.body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response) {
         if (error) {
-          reject(error);
+          console.error(error);
         } else {
-          resolve(JSON.parse(response.body).name);
+          console.log(JSON.parse(response.body).name);
         }
       });
     });
-  });
-
-  Promise.all(characterPromises)
-    .then(names => {
-      names.forEach(name => console.log(name));
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  }
 });
